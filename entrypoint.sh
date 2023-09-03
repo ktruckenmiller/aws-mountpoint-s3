33297bf93f1e8d097e86cd86cfd982b10ceb5a2b
Kevin Truckenmiller <kmtruckenmiller@gmail.com>
2023-09-03 11:14:50 -0500

    Allow other

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -ex
 
 mkdir -p ${S3_MOUNTPOINT}
-mount-s3 -f --allow-delete --allow-root ${S3_BUCKET} ${S3_MOUNTPOINT}
+mount-s3 -f --allow-delete --allow-other ${S3_BUCKET} ${S3_MOUNTPOINT}
